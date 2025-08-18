b7f02276202e8453ba59b27ae13f4dd87d365513
yachea <dev.yachea@gmail.com>
2025-08-18 18:11:42 +0900

    [docs] 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { JSX } from 'react';
+import Home from './components/Home';
+import Item from './components/todos/Item';
+import Edit from './components/todos/Edit';
+import List from './components/todos/List';
+import Write from './components/todos/Write';
 
-function App() {
-  return <div>App</div>;
+function App(): JSX.Element {
+  const [todo, setTodo] = useState();
+  const hancleTodoUpdate = {};
+
+  return (
+    <div>
+      <p>생각나는 아이디어를 적어주세요.</p>
+      <Write />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import Write from './todos/Write';
+
+function Home() {
+  // ts 자리
+
+  // tsx 자리
+  return <div></div>;
+}
+
+export default Home;
